3f21900d101ddaeda1a0f60c63997b6546897c9b
pixushi <67912216+pixushi@users.noreply.github.com>
2020-07-06 13:17:34 -0500

    Add files via upload

---
+++ bycvx.m
@@ -0,0 +1,18 @@
+function bet = bycvx(y, x, constr2, lam)
+% use CVX to solve regularized regression
+[n p] = size(x);
+if (sum(abs(constr2))==0)
+	cvx_begin quiet
+    cvx_precision low
+    variable bet(p)
+        minimize( (y-x*bet)'*(y-x*bet)+2*n*lam*norm(bet,1) )
+	cvx_end
+else
+	cvx_begin quiet
+    cvx_precision low
+    variable bet(p)
+        minimize( (y-x*bet)'*(y-x*bet)+2*n*lam*norm(bet,1) )
+        subject to
+        constr2*bet == 0
+	cvx_end
+end
